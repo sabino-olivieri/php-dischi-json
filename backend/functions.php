41211bf4ab8ff89d44fe077c99f3ce83b6530b74
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-05-25 13:51:07 +0200

    refactor: added new functions

--- backend/functions.php
+++ backend/functions.php
@@ -1,5 +1,11 @@
 <?php
-function toggle_favorite($disc_visible)
+/**
+ * Description fa il toggle dei preferiti
+ * @param array $disc_visible array su cui fare il toggle
+ * @param array $disc_list array da inviare per il salvataggio dei cambiamenti
+ * @returns array modificato
+ */
+function toggle_favorite($disc_visible, $disc_list)
 {
     if (isset($_POST["action"]) && $_POST["action"] === "toggle-favorite") {
         $index = $_POST["index"];
@@ -11,6 +17,78 @@ function toggle_favorite($disc_visible)
             }
         }
     }
+    save_changes($disc_list, $disc_visible);
+    return $disc_visible;
+}
+
+
+/**
+ * Description salva i cambiamenti avvenuti nella funzione toggle_favorite
+ * @param array $disc_list array non filtrato
+ * @param array $disc_visible array filtrato e modificato
+ */
+function save_changes ($disc_list, $disc_visible) {
+    foreach ($disc_list as $index => $cur_disc) {
+        
+        foreach ($disc_visible as $key => $cur_visible) {
+
+            if($cur_disc["title"] === $cur_visible["title"] &&
+            $cur_disc["author"] === $cur_visible["author"] &&
+            $cur_disc["year"] === $cur_visible["year"]) {
+                $disc_list[$index] = $disc_visible[$key];
+
+            }
+        }
+    }
+
+    file_put_contents("../dischi.json", json_encode($disc_list));
+}
+
+/**
+ * Description restituisce array filtrato
+ * @param array $disc_visible array da filtrare
+ * @returns array filtrato
+ */
+function show_filtered_array($disc_visible) {
+
+
+    if ((isset($_GET["filter"]) && $_GET["filter"] === "filter-favorite") ||
+    (isset($_POST["filter"]) && $_POST["filter"] === "filter-favorite")) {
+
+        $favorite = true;
+        $disc_visible = get_filtered_array($disc_visible, $favorite);
+        
+    } elseif ((isset($_GET["filter"]) && $_GET["filter"] === "filter-unfavorite") ||
+    (isset($_POST["filter"]) && $_POST["filter"] === "filter-unfavorite")) {
+
+        $favorite = false;
+        $disc_visible = get_filtered_array($disc_visible, $favorite);
+
+        
+    } else {
+        $disc_visible = $disc_visible;
+    }
 
     return $disc_visible;
+} 
+
+/**
+ * Description restituisce array dei preferiti e non in base al valore di $favorite
+ * @param array $disc_visible array da filtrare
+ * @param boolean $favorite flag se true mostra i preferiti altrimenti i non preferiti
+ * @returns array filtrato
+ */
+function get_filtered_array ($disc_visible, $favorite) {
+
+    $temp_list = [];
+    
+    foreach ($disc_visible as $key => $cur_disc) {
+                
+        if(isset($cur_disc["favorite"]) && $cur_disc["favorite"] === $favorite) {
+            $temp_list[] = $cur_disc;
+        }
+        
+    }
+
+    return $temp_list;
 }
--- backend/server.php
+++ backend/server.php
@@ -7,77 +7,16 @@
 
     $disc_visible = $disc_list;
 
-    if ((isset($_GET["filter"]) && $_GET["filter"] === "filter-favorite") ||
-    (isset($_POST["filter"]) && $_POST["filter"] === "filter-favorite")) {
-        $temp_list = [];
+    $disc_visible = show_filtered_array($disc_visible);
 
-            foreach ($disc_visible as $key => $cur_disc) {
-                
-                if(isset($cur_disc["favorite"]) && $cur_disc["favorite"]) {
-                    $temp_list[] = $cur_disc;
-                }
-                
-            }
+    $disc_visible = toggle_favorite($disc_visible, $disc_list);
 
-            $disc_visible = $temp_list;
-        
-    } elseif ((isset($_GET["filter"]) && $_GET["filter"] === "filter-unfavorite") ||
-    (isset($_POST["filter"]) && $_POST["filter"] === "filter-unfavorite")) {
-        $temp_list = [];
-
-            foreach ($disc_visible as $key => $cur_disc) {
-                
-                if(!isset($cur_disc["favorite"])) {
-                    $temp_list[] = $cur_disc;
-                } elseif (isset($cur_disc["favorite"]) && !$cur_disc["favorite"]){
-                    $temp_list[] = $cur_disc;
-                }
-                
-            }
-
-            $disc_visible = $temp_list;
-        
-    } elseif ((isset($_GET["filter"]) && $_GET["filter"] === "filter-all") ||
-    (isset($_POST["filter"]) && $_POST["filter"] === "filter-all")) {
-        $disc_visible = $disc_list;
-    }
-
-    $disc_visible = toggle_favorite($disc_visible);
-
-
-    // if(isset($_POST["action"]) && $_POST["action"] === "toggle-favorite") {
-    //     $index = $_POST["index"];
-    //     if(!empty($disc_visible)) {
-    //     if(isset($disc_visible[$index]["favorite"])){
-    //         $disc_visible[$index]["favorite"] = !$disc_visible[$index]["favorite"];
-    //     } else  {
-    //         $disc_visible[$index]["favorite"] = true;
-    //     }
-    // }
-    // }
 
     $api_resp = [
         "response" => $disc_visible,
         "status" => "success"
     ];
 
-    //aggiorno disc_list prima di salvare
-    foreach ($disc_list as $index => $cur_disc) {
-        
-        foreach ($disc_visible as $key => $cur_visible) {
-
-            if($cur_disc["title"] === $cur_visible["title"] &&
-            $cur_disc["author"] === $cur_visible["author"] &&
-            $cur_disc["year"] === $cur_visible["year"]) {
-                // var_dump($disc_list[$index]);
-                // var_dump($disc_visible[$key]);
-                $disc_list[$index] = $disc_visible[$key];
-
-            }
-        }
-    }
-
-    file_put_contents("../dischi.json", json_encode($disc_list));
 
     header("Content-Type: application/json");
     echo json_encode($api_resp);
